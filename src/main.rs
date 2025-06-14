e9c28783c168bbc0d16936123480e8932ace96dc
Manish Pardhan <manish@instaastro.in>
2025-06-14 16:12:58 +0530

    program to find the first word of the string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
+
+    let sentence =String::from("This is my first Rust program");
+
+    let first_word=get_first_word(sentence);
+
+    println!("First word of the String is {}", first_word);
+
+    let string= String::from("Hello world");
+    println!("{}",string);
     println!("Hello, world!");
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans=String::from("");
+    for char in sentence.chars(){
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+    return  ans;
 }
