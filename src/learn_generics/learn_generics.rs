dca61d4c117b1a91a4e3a0e1a9c6c1a90bccc780
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-06-23 16:50:02 +0530

    implement generics

--- src/learn_generics/learn_generics.rs
+++ src/learn_generics/learn_generics.rs
@@ -1,6 +1,6 @@
 use std::vec;
 
-pub fn largest_i32(list:&[i32]) -> &i32{
+pub fn largest<A : std::cmp::PartialOrd>(list:&[A]) -> &A{
     let mut largest = &list[0];
 
     for i in list {
@@ -13,6 +13,14 @@ pub fn largest_i32(list:&[i32]) -> &i32{
 
 pub fn gen_fun(){
     let list: Vec<i32> = vec![1,2,3,4];
-    let largest_num = largest_i32(&list);
-    println!("Largest Number in the list is {}", largest_num)
+
+    let list_2 =vec![23.2, 14.8, 33.4,88.9];
+
+    let largest_num = largest(&list);
+
+    // let largest_num_2 = largest_i32(&list_2) Now we can use the same function because it has different return type
+    // to resolve this proble we will use generics
+    let largest_num_2 = largest(&list_2);
+
+    println!("Largest Number in the lists are {}, {}", largest_num, largest_num_2)
 }
